39cc59f2700f4c59764109d717148f8870a91237
keeki-fami <towatowa3939keeki@gmail.com>
2025-09-01 12:56:44 +0900

    カードテンプレ作成

---
+++ coupon/coupon/Card/CardView.swift
@@ -0,0 +1,83 @@
+//
+//  CardView.swift
+//  coupon
+//
+//  Created by 桜田聖和 on 2025/08/31.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View{
+        Button(action: {
+            // Todo:ModalViewに遷移
+            print("ボタンが押されました")
+        }, label: {
+            cardView
+                .frame(width:300,height:200)
+                .cornerRadius(5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray, lineWidth: 2)
+                )
+        })
+        .contentShape(RoundedRectangle(cornerRadius: 5))
+    }
+}
+
+
+    var cardView: some View{
+        ZStack{
+            HStack() {
+                Image("myFace")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width:150, height:200)
+                    .mask(alignment: .top) {
+                        LinearGradient(
+                            gradient: .init(colors: [.white, .clear]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                    }
+                Spacer()
+                VStack(alignment:.leading, spacing:10) {
+                    Spacer()
+                    Text("タイトル")
+                        .font(.system(size :20))
+                        .foregroundColor(.black)
+                    Spacer()
+                    VStack{
+                        HStack{
+                            Image(systemName:"globe")
+                            Text("サンプル１")
+                                .foregroundColor(.gray)
+                                .font(.system(size :13))
+                            Spacer()
+                        }
+                        HStack{
+                            Image(systemName:"globe")
+                            Text("サンプル１")
+                                .foregroundColor(.gray)
+                                .font(.system(size :13))
+                            Spacer()
+                        }
+                        HStack{
+                            Image(systemName:"globe")
+                            Text("サンプル１")
+                                .foregroundColor(.gray)
+                                .font(.system(size :13))
+                            Spacer()
+                        }
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+    }
+
+
+#Preview{
+    CardView()
+}
--- coupon/coupon/CouponList/CouponListView.swift
+++ coupon/coupon/CouponList/CouponListView.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 struct CouponListView: View {
+    let x: [Int] = [0,1,2,3,4,5]
     var body: some View {
         NavigationStack{
-            VStack{
-                Text("これはCouponListViewです")
+            ScrollView{
+                VStack{
+                    ForEach(x,id:\.self){_ in
+                        CardView()
+                            .padding(10)
+                    }
+                }
             }
             .navigationTitle("クーポン")
         }
