c6790c5a0e31cd3ee2c1481e51a0754528643778
keeki-fami <towatowa3939keeki@gmail.com>
2025-09-07 01:27:57 +0900

    coredata,model作成

--- coupon/coupon/CouponList/CouponListView.swift
+++ coupon/coupon/CouponList/CouponListView.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 struct CouponListView: View {
     let x: [Int] = [0,1,2,3,4,5]
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \CardModel.limit, ascending: true)],
+        animation: .default
+    ) private var coupons: FetchedResults<CardModel>
+    
     var body: some View {
         NavigationStack{
             ScrollView{
--- coupon/coupon/couponApp.swift
+++ coupon/coupon/couponApp.swift
@@ -10,10 +10,12 @@ import SwiftUI
 @main
 struct couponApp: App {
     @StateObject private var isEditView = IsEditView()
+    let persistenceController = PersistenceController.shared
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(isEditView)
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
